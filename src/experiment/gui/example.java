13bd6bf882dba5840ae33f7159e4b0c37155485d
Zahi Mizrahi <zahimizra1@gmail.com>
2016-12-08 23:19:57 +0200

    using lambda expression for user events #55

--- src/experiment/gui/example.java
+++ src/experiment/gui/example.java
@@ -2,43 +2,41 @@
 package experiment.gui;
 
 import javafx.application.Application;
-import javafx.stage.Stage; 
+import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
-import javafx.event.EventHandler;
-import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
+//import javafx.event.ActionEvent;
 
-public class example extends Application implements EventHandler<ActionEvent> {
- 
-	Button button1; 
-	public static void main (String[] args) {
-		launch(args); 
+public class example extends Application {
+
+	Button button1;
+
+	public static void main(String[] args) {
+		launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		primaryStage.setTitle("First Window"); 
-		
-		button1 = new Button ();
-		
-		button1.setText("Accept");
-		button1.setOnAction(this); 
+		primaryStage.setTitle("First Window");
 
-		
-		StackPane layout = new StackPane ();
-		
+		button1 = new Button();
+
+		button1.setText("Accept");
+		button1.setOnAction(e -> {
+			
+		System.out.println("OK");
+		System.out.println("Got it?");
+		});
+
+		StackPane layout = new StackPane();
+
 		layout.getChildren().add(button1);
-		Scene scene = new Scene (layout,300,250);
+		Scene scene = new Scene(layout, 300, 250);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
-	
 
-	@Override
-	public void handle(ActionEvent ¢) {
-		if (¢.getSource().equals(button1))
-			System.out.println("OK!");
-}
 }
