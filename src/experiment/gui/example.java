df863a37e88e22bfc82e011627b78d950d3cef7d
Zahi Mizrahi <zahimizra1@gmail.com>
2016-12-08 22:46:00 +0200

    fixed handle for user events #55

--- src/experiment/gui/example.java
+++ src/experiment/gui/example.java
@@ -12,7 +12,6 @@ import javafx.event.ActionEvent;
 public class example extends Application implements EventHandler<ActionEvent> {
  
 	Button button1; 
-	Button button2; 
 	public static void main (String[] args) {
 		launch(args); 
 	}
@@ -23,18 +22,14 @@ public class example extends Application implements EventHandler<ActionEvent> {
 		primaryStage.setTitle("First Window"); 
 		
 		button1 = new Button ();
-		button2 = new Button(); 
 		
 		button1.setText("Accept");
 		button1.setOnAction(this); 
-		
-		button2.setText("Accept");
-		button2.setOnAction(this); 
+
 		
 		StackPane layout = new StackPane ();
 		
 		layout.getChildren().add(button1);
-		layout.getChildren().add(button2);
 		Scene scene = new Scene (layout,300,250);
 		primaryStage.setScene(scene);
 		primaryStage.show();
@@ -45,7 +40,5 @@ public class example extends Application implements EventHandler<ActionEvent> {
 	public void handle(ActionEvent ¢) {
 		if (¢.getSource().equals(button1))
 			System.out.println("OK!");
-		if (¢.getSource().equals(button2))
-			System.out.println("You Can't!");
-	}
+}
 }
