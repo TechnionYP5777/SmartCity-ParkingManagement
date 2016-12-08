92ac64d567c64eb3f25bb7f1e06e46e3c4bbf19a
Zahi Mizrahi <zahimizra1@gmail.com>
2016-12-08 22:44:33 +0200

    creating handle for user events #55

--- src/experiment/gui/example.java
+++ src/experiment/gui/example.java
@@ -6,11 +6,13 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.event.EventHandler;
+import javafx.event.ActionEvent;
 
-public class example extends Application{
+public class example extends Application implements EventHandler<ActionEvent> {
  
-	Button button; 
-	
+	Button button1; 
+	Button button2; 
 	public static void main (String[] args) {
 		launch(args); 
 	}
@@ -18,13 +20,32 @@ public class example extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
-		primaryStage.setTitle("Shahar The King"); 
-		button = new Button ();
-		button.setText("Click Me!");
+		primaryStage.setTitle("First Window"); 
+		
+		button1 = new Button ();
+		button2 = new Button(); 
+		
+		button1.setText("Accept");
+		button1.setOnAction(this); 
+		
+		button2.setText("Accept");
+		button2.setOnAction(this); 
+		
 		StackPane layout = new StackPane ();
-		layout.getChildren().add(button);
+		
+		layout.getChildren().add(button1);
+		layout.getChildren().add(button2);
 		Scene scene = new Scene (layout,300,250);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+	}
+	
+
+	@Override
+	public void handle(ActionEvent ¢) {
+		if (¢.getSource().equals(button1))
+			System.out.println("OK!");
+		if (¢.getSource().equals(button2))
+			System.out.println("You Can't!");
 	}
 }
