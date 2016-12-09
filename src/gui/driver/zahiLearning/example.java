c4396806493266ebc0c62a351b941d96635a538b
Zahi Mizrahi <zahimizra1@gmail.com>
2016-12-10 00:22:03 +0200

    gridpane #55

--- src/gui/driver/zahiLearning/example.java
+++ src/gui/driver/zahiLearning/example.java
@@ -6,12 +6,14 @@
 package gui.driver.zahiLearning;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+;
 
 public class example extends Application {
 
@@ -25,26 +27,29 @@ public class example extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		window = primaryStage;
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(10, 10, 10, 10));
+		grid.setVgap(8);
+		grid.setHgap(10);
 		
-		HBox topMenu = new HBox();
-		Button button1 = new Button("File");
-		Button button2 = new Button("Edit");
-		Button button3 = new Button ("View");
-		topMenu.getChildren().addAll(button1,button2, button3); 
+		Label user = new Label ("Username");
+		TextField nameInput = new TextField("Zahi"); 
 		
-		VBox leftMenu = new VBox();
-		Button button4 = new Button("A");
-		Button button5 = new Button("B");
-		Button button6 = new Button ("C");
-		leftMenu.getChildren().addAll(button4,button5, button6); 
+		Label pass = new Label ("Password");
+		TextField passInput = new TextField();
+		passInput.setPromptText("password");
 		
-		 BorderPane borderPane = new BorderPane (); 
-		 borderPane.setTop(topMenu);
-		 borderPane.setLeft(leftMenu);
-		scene = new Scene(borderPane, 600, 300);
-
-		window.setTitle("The Window");
+		Button button = new Button ("Login");
+		GridPane.setConstraints (user,0,0);
+		GridPane.setConstraints(nameInput, 1,0); 
+		GridPane.setConstraints(pass, 0,1);
+		GridPane.setConstraints(passInput, 1,1);
+		GridPane.setConstraints(button,1,2); 
+		
+		grid.getChildren().addAll(user,nameInput,pass,passInput,button);
+		Scene scene = new Scene (grid);
 		window.setScene(scene);
+		window.setTitle("The Window");
 		window.show();
 	}
 
