39b81e3048f46468a8b36fb910d98aac569f50c1
Zahi Mizrahi <zahimizra1@gmail.com>
2016-12-10 00:07:49 +0200

    embedded layouts #55

--- src/gui/driver/zahiLearning/example.java
+++ src/gui/driver/zahiLearning/example.java
@@ -9,13 +9,14 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class example extends Application {
 
 	Stage window;
-	Scene scene1, scene2;
+	Scene scene;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -23,28 +24,28 @@ public class example extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
 		window = primaryStage;
-		window.setOnCloseRequest(e -> {
-			e.consume();
-			closeProgram();
-		});
-		Button button = new Button("Close Progress!");
-		button.setOnAction(e -> closeProgram());
-		StackPane layout1 = new StackPane();
-
-		layout1.getChildren().addAll(button);
-		scene1 = new Scene(layout1, 600, 300);
+		
+		HBox topMenu = new HBox();
+		Button button1 = new Button("File");
+		Button button2 = new Button("Edit");
+		Button button3 = new Button ("View");
+		topMenu.getChildren().addAll(button1,button2, button3); 
+		
+		VBox leftMenu = new VBox();
+		Button button4 = new Button("A");
+		Button button5 = new Button("B");
+		Button button6 = new Button ("C");
+		leftMenu.getChildren().addAll(button4,button5, button6); 
+		
+		 BorderPane borderPane = new BorderPane (); 
+		 borderPane.setTop(topMenu);
+		 borderPane.setLeft(leftMenu);
+		scene = new Scene(borderPane, 600, 300);
 
 		window.setTitle("The Window");
-		window.setScene(scene1);
+		window.setScene(scene);
 		window.show();
-	}
-
-	private void closeProgram() {
-		if (ConfirmBox.display("title", "Are you sure you want to exit?"))
-			window.close();
-
 	}
 
 }
