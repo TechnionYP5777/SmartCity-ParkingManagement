9f08b0184c5ce53a91ec9e7f724cd737a7eb5778
zahimizrahi <zahimizrahi1@gmail.com>
2016-12-10 13:27:20 +0200

    listView learned. #55

--- src/gui/driver/zahiLearning/example.java
+++ src/gui/driver/zahiLearning/example.java
@@ -6,20 +6,21 @@
 package gui.driver.zahiLearning;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.layout.VBox;
-;
 
 public class example extends Application {
 
 	Stage window;
 	Scene scene;
 	Button button; 
-	ComboBox<String> comboBox; 
+	ListView<String> listView; 
 
 	public static void main(String[] args) {
 		launch(args);
@@ -30,24 +31,29 @@ public class example extends Application {
 		window = primaryStage;
 		window.setTitle("choicebox example");
 		
-		comboBox = new ComboBox<> (); 
-		comboBox.getItems().addAll ("Pizza", "Sushi", "Hamburger", "Fries");
+		listView = new ListView<> (); 
+		listView.getItems().addAll ("Pizza", "Sushi", "Hamburger", "Fries");
 		
-		comboBox.setPromptText ("What is your favorite food?");
-
+		listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 		//Button
 		button = new Button ("Click Me!"); 
-		button.setOnAction(e -> System.out.println(comboBox.getValue()));
-		
-		comboBox.setOnAction(e -> System.out.println("User Selected: "+comboBox.getValue()));
+		button.setOnAction(e -> buttonClicked());
 	    VBox layout = new VBox (10); 
 	    layout.setPadding(new Insets(20,20,20,20));
-	    layout.getChildren().addAll(comboBox, button);
+	    layout.getChildren().addAll(listView, button);
 		scene = new Scene (layout, 300,250);
 		
 		window.setScene(scene);
 		window.show();
 	}
 	
+	private void buttonClicked () {
+		String message = "";
+		ObservableList<String> foods = listView.getSelectionModel().getSelectedItems(); 
+		for (String food : foods)
+			message += food + "\n";
+		System.out.println(message);
+		
+	}
 
 }
