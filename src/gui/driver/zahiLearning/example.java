da5e316c4709ea18fe3836508b5ad07536a1665e
Zahi Mizrahi <zahimizra1@gmail.com>
2016-12-10 12:30:56 +0200

    learning choicebox #55

--- src/gui/driver/zahiLearning/example.java
+++ src/gui/driver/zahiLearning/example.java
@@ -11,6 +11,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.VBox;
 ;
 
@@ -27,30 +28,24 @@ public class example extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		window = primaryStage;
-		window.setTitle("checkbox example");
+		window.setTitle("choicebox example");
 		
 		//CheckBox
-		CheckBox box1 = new CheckBox ("Option No.1"); 
-		CheckBox box2 = new CheckBox ("Option No.2"); 
-		box1.setSelected(true);
+		ChoiceBox<String> choiceBox= new ChoiceBox<>();
+
+		choiceBox.getItems().addAll("Option No.1", "Option No.2", "Option No.3");
+		choiceBox.setValue("Option No.1");
 		//Button
 		button = new Button ("Click Me!"); 
-		button.setOnAction( e -> handleOptions(box1,box2));
+		button.setOnAction (e -> System.out.println(choiceBox.getValue())); 
 	    VBox layout = new VBox (10); 
 	    layout.setPadding(new Insets(20,20,20,20));
-	    layout.getChildren().addAll(box1, box2, button);
+	    layout.getChildren().addAll(choiceBox, button);
 		scene = new Scene (layout, 300,250);
 		
 		window.setScene(scene);
 		window.show();
 	}
 	
-	private void handleOptions (CheckBox b1, CheckBox b2) {
-		String message = "User chose:\n" + (!b1.isSelected() ? "" : "option no.1\n"); 
-		if (b2.isSelected())
-			message += "option no.2\n";
-		
-		System.out.println(message);
-		
-	}
+
 }
