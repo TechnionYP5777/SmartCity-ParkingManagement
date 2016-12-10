bfddefa150a32037a099b052c5958cf3e41d3c07
zahimizrahi <zahimizrahi1@gmail.com>
2016-12-10 13:14:14 +0200

    combobox #55

--- src/gui/driver/zahiLearning/example.java
+++ src/gui/driver/zahiLearning/example.java
@@ -10,8 +10,7 @@ import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.VBox;
 ;
 
@@ -20,6 +19,7 @@ public class example extends Application {
 	Stage window;
 	Scene scene;
 	Button button; 
+	ComboBox<String> comboBox; 
 
 	public static void main(String[] args) {
 		launch(args);
@@ -30,17 +30,19 @@ public class example extends Application {
 		window = primaryStage;
 		window.setTitle("choicebox example");
 		
-		//CheckBox
-		ChoiceBox<String> choiceBox= new ChoiceBox<>();
+		comboBox = new ComboBox<> (); 
+		comboBox.getItems().addAll ("Pizza", "Sushi", "Hamburger", "Fries");
+		
+		comboBox.setPromptText ("What is your favorite food?");
 
-		choiceBox.getItems().addAll("Option No.1", "Option No.2", "Option No.3");
-		choiceBox.setValue("Option No.1");
 		//Button
 		button = new Button ("Click Me!"); 
-		button.setOnAction (e -> System.out.println(choiceBox.getValue())); 
+		button.setOnAction(e -> System.out.println(comboBox.getValue()));
+		
+		comboBox.setOnAction(e -> System.out.println("User Selected: "+comboBox.getValue()));
 	    VBox layout = new VBox (10); 
 	    layout.setPadding(new Insets(20,20,20,20));
-	    layout.getChildren().addAll(choiceBox, button);
+	    layout.getChildren().addAll(comboBox, button);
 		scene = new Scene (layout, 300,250);
 		
 		window.setScene(scene);
