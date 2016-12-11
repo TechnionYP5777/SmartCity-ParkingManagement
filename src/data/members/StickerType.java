8fd7bac27d1e2b613db70502f7e87f402da15a77
= <dcohen484@gmail.com>
2016-12-12 00:56:24 +0200

    reupload the User class because of problam #44

---
+++ src/data/members/StickerType.java
@@ -0,0 +1,5 @@
+package data.members;
+
+public enum StickerType {
+	BLUE,BLUE_WHITE,YELLOW,GREEN,GREEN_RED,RED,ORANGE
+}
---
+++ src/data/members/User.java
@@ -0,0 +1,58 @@
+package data.members;
+
+/**
+ * @Author DavidCohen55
+ */
+	
+public class User {
+	
+	private String name;
+	private String password;
+	private String phoneNumber;
+	private String carNumber;
+	private StickerType sticker; 
+
+	public User(String name,String password,String phoneNumber,String carNumber,StickerType type){
+			this.name=name;
+			this.password=password;
+			this.phoneNumber=phoneNumber;
+			this.carNumber=carNumber;
+			this.sticker=type;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public String getCarNumber() {
+		return carNumber;
+	}
+	
+	public void setCarNumber(String carNumber) {
+		this.carNumber = carNumber;
+	}
+	
+	public StickerType getSticker() {
+		return sticker;
+	}
+	
+	public void setSticker(StickerType type) {
+		this.sticker = type;
+	}
+}
