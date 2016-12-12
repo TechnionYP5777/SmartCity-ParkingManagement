1430db7e4e82cecb4d44352888f246a1d2b0d124
Or Troyaner <or.troyaner@gmail.com>
2016-12-12 02:43:09 +0200

    added switch scenes functionality #61

--- src/gui/manager/OrLearning/CreatingABasicWindow.java
+++ src/gui/manager/OrLearning/CreatingABasicWindow.java
@@ -9,7 +9,8 @@ import javafx.stage.Stage;
 
 public class CreatingABasicWindow extends Application implements EventHandler<ActionEvent> {
 	Stage window;
-	Button myButton;
+	Button myButton, switchToScene2, switchToScene1;
+	Scene scene1, scene2;
 	
 	public static void main(String[] args) {
 		launch(args);
@@ -23,11 +24,26 @@ public class CreatingABasicWindow extends Application implements EventHandler<Ac
 		myButton = new Button();
 		myButton.setText("Click Here");
 		myButton.setOnAction(this);
-		StackPane layout = new StackPane();
-		layout.getChildren().add(myButton);
-		Scene scene = new Scene(layout, 500, 500);
 		
-		window.setScene(scene);
+		//Change scene buttons, using lambda action event
+		switchToScene2 = new Button();
+		switchToScene2.setText("Switch to 2");
+		switchToScene2.setOnAction(e -> window.setScene(scene2));
+		
+		switchToScene1 = new Button();
+		switchToScene1.setText("Switch to 1");
+		switchToScene1.setOnAction(e -> window.setScene(scene1));
+		
+		//Layouts
+		StackPane layout = new StackPane();
+		layout.getChildren().add(switchToScene2);
+		scene1 = new Scene(layout, 500, 500);
+		
+		StackPane layout2 = new StackPane();
+		layout2.getChildren().add(switchToScene1);
+		scene2 = new Scene(layout2, 500, 500);
+		
+		window.setScene(scene1);
 		window.show();
 	}
 	
