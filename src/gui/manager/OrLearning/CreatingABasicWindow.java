78a8955ca46d7598ff8350ce39cbb90b0202b777
Or Troyaner <or.troyaner@gmail.com>
2016-12-12 02:10:31 +0200

    After overviewing event handler #61

--- src/gui/manager/OrLearning/CreatingABasicWindow.java
+++ src/gui/manager/OrLearning/CreatingABasicWindow.java
@@ -1,12 +1,16 @@
 package gui.manager.OrLearning;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class CreatingABasicWindow extends Application {
+public class CreatingABasicWindow extends Application implements EventHandler<ActionEvent> {
 	Stage window;
+	Button myButton;
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -16,12 +20,21 @@ public class CreatingABasicWindow extends Application {
 		window=mainStage;
 		window.setTitle("Yearly Project is awesome");
 		
+		myButton = new Button();
+		myButton.setText("Click Here");
+		myButton.setOnAction(this);
 		StackPane layout = new StackPane();
-		layout.getChildren().add(new Button("DO NOT CLICK HERE"));
+		layout.getChildren().add(myButton);
 		Scene scene = new Scene(layout, 500, 500);
 		
 		window.setScene(scene);
 		window.show();
+	}
+	
+	@Override
+	public void handle(ActionEvent ¢) {
+		if (¢.getSource()==myButton)
+			System.out.println("That's worked");
 	}
 	
 }
