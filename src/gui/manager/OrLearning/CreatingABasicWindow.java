577e4a3d60c1e299d993f7cb812aac4b91450dd2
Or Troyaner <or.troyaner@gmail.com>
2016-12-12 04:13:07 +0200

    Fixed a minor warning #61

--- src/gui/manager/OrLearning/CreatingABasicWindow.java
+++ src/gui/manager/OrLearning/CreatingABasicWindow.java
@@ -10,7 +10,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
