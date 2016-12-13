7c700679669cf7b78c5ee0615770c9952cd121f7
zahimizrahi <zahimizrahi1@gmail.com>
2016-12-13 23:37:48 +0200

    added message for sucessfully log in #20

--- src/gui/driver/app/Login.java
+++ src/gui/driver/app/Login.java
@@ -7,11 +7,14 @@ package gui.driver.app;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Login extends Application {
@@ -27,15 +30,15 @@ public class Login extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		window = primaryStage;
 		GridPane grid = new GridPane();
-		grid.setPadding(new Insets(10, 10, 10, 10));
+		grid.setPadding(new Insets(20, 20, 20, 20));
 		grid.setVgap(8);
 		grid.setHgap(10);
 
 		Label user = new Label("Username");
-		TextField nameInput = new TextField("Username");
-
+		TextField nameInput = new TextField();
+		nameInput.setPromptText("username");
 		Label pass = new Label("Password");
-		TextField passInput = new TextField("Password");
+		TextField passInput = new TextField();
 		passInput.setPromptText("password");
 		Label forgotPass = new Label ("Forgot Password?");
 		
@@ -45,13 +48,35 @@ public class Login extends Application {
 		GridPane.setConstraints(pass, 0, 1);
 		GridPane.setConstraints(passInput, 1, 1);
 		GridPane.setConstraints(button, 1, 2);
-		GridPane.setConstraints(forgotPass,2,2);
+		GridPane.setConstraints(forgotPass,3,2);
 
 		grid.getChildren().addAll(user, nameInput, pass, passInput, button,forgotPass);
+		
+		button.setOnAction( e-> display("Successful", "You have successfully logged in!"));
 		Scene scene = new Scene(grid);
 		window.setScene(scene);
 		window.setTitle("Login");
 		window.show();
+	}
+	
+	
+	public static void display (String title, String message) {
+		Stage messageBox = new Stage();
+		messageBox.initModality(Modality.APPLICATION_MODAL);
+		messageBox.setTitle(title); 
+		messageBox.setMinWidth(250);
+		messageBox.setMinHeight(50);
+		Label label = new Label (message); 
+		Button closeButton = new Button ("Close");
+		closeButton.setOnAction(e -> messageBox.close());
+		
+		VBox layout = new VBox (10); 
+		layout.getChildren().addAll(label, closeButton); 
+		layout.setAlignment(Pos.CENTER);
+		
+		Scene scene = new Scene(layout); 
+		messageBox.setScene(scene);
+		messageBox.showAndWait();
 	}
 
 }
