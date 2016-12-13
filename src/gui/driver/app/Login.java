bada89253c6a0f530fddf7a56b64b05fb2e1cc96
zahimizrahi <zahimizrahi1@gmail.com>
2016-12-14 00:29:56 +0200

    added graphic title for screen #20

--- src/gui/driver/app/Login.java
+++ src/gui/driver/app/Login.java
@@ -6,15 +6,22 @@
 package gui.driver.app;
 
 import javafx.application.Application;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -35,23 +42,38 @@ public class Login extends Application {
 		grid.setVgap(8);
 		grid.setHgap(10);
 
+		//title 
+		DropShadow shadow = new DropShadow(); 
+		shadow.setOffsetY(4.0);
+		shadow.setColor(Color.color(0.4f, 0.4f, 0.4f));
+		Label title = new Label();
+		title.setEffect(shadow);
+		title.setTextFill(Color.ROYALBLUE);
+		title.setTextAlignment(TextAlignment.CENTER);
+		title.setText("Login");
+		title.setFont(Font.font(null, FontWeight.BOLD, 48));
+
+		//user
 		Label user = new Label("Username");
 		TextField nameInput = new TextField();
 		nameInput.setPromptText("username");
+		
+		//password
 		Label pass = new Label("Password");
 		PasswordField passInput = new PasswordField();
 		passInput.setPromptText("password");
 		Label forgotPass = new Label ("Forgot Password?");
 		
 		Button button = new Button("Login");
-		GridPane.setConstraints(user, 0, 0);
-		GridPane.setConstraints(nameInput, 1, 0);
-		GridPane.setConstraints(pass, 0, 1);
-		GridPane.setConstraints(passInput, 1, 1);
-		GridPane.setConstraints(button, 1, 2);
-		GridPane.setConstraints(forgotPass,3,2);
+		GridPane.setRowIndex(title, 0);
+		GridPane.setConstraints(user, 0, 1);
+		GridPane.setConstraints(nameInput, 1, 1);
+		GridPane.setConstraints(pass, 0, 2);
+		GridPane.setConstraints(passInput, 1, 2);
+		GridPane.setConstraints(button, 1, 3);
+		GridPane.setConstraints(forgotPass,3,3);
 
-		grid.getChildren().addAll(user, nameInput, pass, passInput, button,forgotPass);
+		grid.getChildren().addAll(title,user, nameInput, pass, passInput, button,forgotPass);
 		
 		button.setOnAction( e-> display("Successful", "You have successfully logged in!"));
 		Scene scene = new Scene(grid);
