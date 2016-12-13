ef813a458b5d2407989bcdbb63b1c2f77fdd7de3
Shahar-Y <shya95@gmail.com>
2016-12-13 14:43:36 +0200

    @Shahar-Y Took care of warnings

---
+++ src/gui/driver/shaharTesting/MainScreen.java
@@ -0,0 +1,31 @@
+package gui.driver.shaharTesting;
+
+import javafx.application.Application;
+//import javafx.event.ActionEvent;
+//import javafx.event.EventHandler;
+//import javafx.geometry.Insets;
+//import javafx.geometry.Pos;
+//import javafx.scene.Scene;
+//import javafx.scene.control.Button;
+//import javafx.scene.control.ListView;
+//import javafx.scene.layout.StackPane;
+//import javafx.scene.layout.AnchorPane;
+//import javafx.scene.layout.BorderPane;
+//import javafx.scene.layout.HBox;
+//import javafx.scene.layout.StackPane;
+//import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MainScreen extends Application{
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
