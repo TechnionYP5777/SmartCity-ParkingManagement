76ff7b36d4d695a937a5d10bfd308655c9f47c20
shaysegal <siglons@gmail.com>
2016-12-14 22:25:37 +0200

    add serverConnecrion test, comment Navigation test as dani and david suggested

--- test/logic/NavigationTest.java
+++ test/logic/NavigationTest.java
@@ -7,14 +7,14 @@ public class NavigationTest {
 	@Test
 	public void test1() {
 		// Might fail while there is traffic in the technion.
-		Assert.assertEquals(317, Navigation.getDistance(32.777400, 35.020178, 32.777544, 35.020092, false));
+		//Assert.assertEquals(317, Navigation.getDistance(32.777400, 35.020178, 32.777544, 35.020092, false));
 	}
 	@Test
 	public void test2() {
-		Assert.assertEquals(0, Navigation.getClosestParkingArea(32.777297, 35.019971, false));
+		//Assert.assertEquals(0, Navigation.getClosestParkingArea(32.777297, 35.019971, false));
 	}
 	@Test
 	public void test3() {
-		Assert.assertEquals(1, Navigation.getClosestParkingArea(32.778848, 35.017095, false));
+		//Assert.assertEquals(1, Navigation.getClosestParkingArea(32.778848, 35.017095, false));
 	}
 }
