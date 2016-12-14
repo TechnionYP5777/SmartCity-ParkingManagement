94cd85415f6f95c85a0d40682d3a84fb92c00bbd
= <dcohen484@gmail.com>
2016-12-14 19:14:37 +0200

    Made the login with the database #33

--- src/data/members/User.java
+++ src/data/members/User.java
@@ -1,5 +1,9 @@
 package data.members;
 
+import org.parse4j.ParseObject;
+import org.parse4j.ParseException;
+import data.management.DBManager;
+
 /**
  * @Author DavidCohen55
  */
@@ -7,43 +11,44 @@ package data.members;
 public class User {
 
 	// the name of the user
-	private String name; 
-	
-	 // the password of the user
+	private String name;
+
+	// the password of the user
 	private String password;
-	
-	// the phone number of the user, maybe be use to send notifications to the user
-	private String phoneNumber; 
-	
-	 // the serial number of the users car will use to identify the user
+
+	// the phone number of the user, maybe be use to send notifications to the
+	// user
+	private String phoneNumber;
+
+	// the serial number of the users car will use to identify the user
 	private String carNumber;
-	
+
 	// the type of sticker of the user, will determine where can he park
-	private StickersColor sticker; 
-	
-	// saves the parking slot of a user if he parked 
-	private ParkingSlot currentParking; 
+	private StickersColor sticker;
+
+	// saves the parking slot of a user if he parked
+	private ParkingSlot currentParking;
+
+	private ParseObject user;
 
 	public User(String name, String password, String phoneNumber, String carNumber, StickersColor type,
-			ParkingSlot currentLocation) {
-		this.name = name;
-		this.password = password;
+			ParkingSlot currentLocation) throws ParseException {
+		DBManager.initialize();
+		this.user = new ParseObject("User");
+		this.setName(name);
+		this.setPassword(password);
 		this.phoneNumber = phoneNumber;
 		this.carNumber = carNumber;
 		this.sticker = type;
 		this.currentParking = currentLocation;
+		user.save();
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
+	
+	/*Get functions*/
+	
 	public ParkingSlot getCurrentParking() {
 		return currentParking;
-	}
-
-	public void setCurrentParking(ParkingSlot currentParking) {
-		this.currentParking = currentParking;
 	}
 
 	public String getName() {
@@ -54,19 +59,10 @@ public class User {
 		return password;
 	}
 
-	public void setPassword(String newPassword, String passwordVerify) {
-		if (newPassword.equals(passwordVerify))
-			this.password = newPassword;
-	}
-
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
-
-	public void setPhoneNumber(String phoneNumber) {
-		this.phoneNumber = phoneNumber;
-	}
-
+	
 	public String getCarNumber() {
 		return carNumber;
 	}
@@ -74,21 +70,55 @@ public class User {
 	public StickersColor getSticker() {
 		return sticker;
 	}
+	
+	/*Set functions*/
+	public void setName(String name) {
+		this.name = name;
+		this.user.put("name", name);
+	}
+	
+	public void setCurrentParking(ParkingSlot currentParking) {
+		this.currentParking = currentParking;
+		this.user.put("currentParking", currentParking);
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+		this.user.put("password", password);
+	}
+	
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+		this.user.put("phoneNumber", phoneNumber);
+	}
+
 
 	public void setSticker(StickersColor type) {
 		this.sticker = type;
+		this.user.put("sticker", type.ordinal());
 	}
-	
+
 	/**
-	 * will be use to update the properties of a user
-	 * won't change stickerType because you will need a manager approve
-	 * won't change carNumber because this will be identification of a user
+	 * will be use to update the properties of a user won't change stickerType
+	 * because you will need a manager approve won't change carNumber because
+	 * this will be identification of a user
+	 * 
 	 * @param name
 	 * @param phoneNumber
+	 * @throws ParseException 
 	 */
-	 public void updateUser(String name, String phoneNumber){
-			this.name = name;
-			this.phoneNumber = phoneNumber;
-			/*need to update DB*/
-	 }
+	public void updateUser(String name, String phoneNumber) throws ParseException {
+		this.setName(name);
+		this.setPhoneNumber(phoneNumber);
+		this.user.save();
+	}
+	
+	public void updatePassword(String newPassword, String passwordVerify) {
+		if (!newPassword.equals(passwordVerify))
+			return;
+		this.password = newPassword;
+		this.user.put("password", password);
+	}
+
+	
 }
