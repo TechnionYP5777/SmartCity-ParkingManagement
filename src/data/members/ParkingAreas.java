bfef300d59d71aae5b075fcc59e36ce1682c2c02
matityahuinba <matityahuinbal@gmail.com>
2016-12-16 11:56:52 +0200

    #35 #32 update ParkingAreas according to DB

--- src/data/members/ParkingAreas.java
+++ src/data/members/ParkingAreas.java
@@ -1,7 +1,15 @@
 package data.members;
 
-import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import org.parse4j.ParseException;
+import org.parse4j.ParseObject;
+import org.parse4j.ParseQuery;
+
+import data.management.DBManager;
+
+import java.util.HashSet;
 
 /**
  * @author Inbal Matityahu
@@ -12,6 +20,15 @@ import java.util.Set;
 
 public class ParkingAreas {
 	private Set<ParkingArea> parkingAreas;
+	private ParseObject areas;
+
+	public ParkingAreas(Set<ParkingArea> parkingAreas) throws ParseException {
+		DBManager.initialize();
+		this.areas = new ParseObject("ParkingAreas");
+		this.parkingAreas = new HashSet<ParkingArea>();
+		this.parkingAreas = parkingAreas;
+		areas.save();
+	}
 
 	public Set<ParkingArea> getParkingAreas() {
 		return parkingAreas;
@@ -19,6 +36,7 @@ public class ParkingAreas {
 
 	public void setParkingAreas(Set<ParkingArea> parkingAreas) {
 		this.parkingAreas = parkingAreas;
+		this.areas.put("parkingAreas", parkingAreas);
 	}
 
 	public ParkingAreas() {
@@ -27,38 +45,51 @@ public class ParkingAreas {
 
 	public void addParkingArea(ParkingArea newParkingArea) {
 		this.parkingAreas.add(newParkingArea);
+		this.areas.put("parkingAreas", this.getParkingAreas());
 	}
 
 	public void removeParkingArea(ParkingArea parkingArea) {
+
+		// search if parkingArea exist
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("ParkingArea");
+		query.whereEqualTo("areaId", parkingArea.getAreaId());
+		try {
+			List<ParseObject> areaList = query.find();
+			if (areaList.size() != 1) {
+				System.out.format("Something went worng while searching for %d", parkingArea.getAreaId());
+				return;
+			}
+
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return;
+		}
+
 		this.parkingAreas.remove(parkingArea);
+		this.areas.put("parkingAreas", this.getParkingAreas());
 	}
 
 	// Return num of taken parking slots by a given area
-	public int getNumOfTakenByArea(ParkingArea area) {
-		if (!this.parkingAreas.contains(area)) {
+	public int getNumOfTakenByArea(ParkingArea a) {
+		if (!this.parkingAreas.contains(a))
 			return 0;
-		}
-		int count = 0;
-		for (ParkingArea currentArea : this.parkingAreas) {
-			if (currentArea.equals(area)) {
-				count = currentArea.getNumOfTakenSlots();
-			}
-		}
-		return count;
+
+		int $ = 0;
+		for (ParkingArea currentArea : this.parkingAreas)
+			if (currentArea.equals(a))
+				$ = currentArea.getNumOfTakenSlots();
+		return $;
 	}
 
 	// Return num of free parking slots by given area
-	public int getNumOfFreeByArea(ParkingArea area) {
-		if (!this.parkingAreas.contains(area)) {
+	public int getNumOfFreeByArea(ParkingArea a) {
+		if (!this.parkingAreas.contains(a))
 			return 0;
-		}
-		int count = 0;
-		for (ParkingArea currentArea : this.parkingAreas) {
-			if (currentArea.equals(area)) {
-				count = currentArea.getNumOfFreeSlots();
-			}
-		}
-		return count;
+		int $ = 0;
+		for (ParkingArea currentArea : this.parkingAreas)
+			if (currentArea.equals(a))
+				$ = currentArea.getNumOfFreeSlots();
+		return $;
 	}
 
 	// Return num of free parking slots
