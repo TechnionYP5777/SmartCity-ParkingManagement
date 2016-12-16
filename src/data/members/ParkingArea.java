fa2113dfaebca6c7158637598dd0b2027ef5f414
matityahuinba <matityahuinbal@gmail.com>
2016-12-16 11:32:05 +0200

    #35 add selectArea+ update queries

--- src/data/members/ParkingArea.java
+++ src/data/members/ParkingArea.java
@@ -1,6 +1,13 @@
 package data.members;
 
+import java.util.List;
 import java.util.Set;
+
+import org.parse4j.ParseException;
+import org.parse4j.ParseObject;
+import org.parse4j.ParseQuery;
+
+import data.management.DBManager;
 
 /**
  * @author Inbal Matityahu
@@ -19,17 +26,35 @@ public class ParkingArea {
 	private Set<ParkingSlot> parkingSlots;
 	private Set<ParkingSlot> freeSlots;
 	private Set<ParkingSlot> takenSlots;
-
-	public ParkingArea(){
-		// will populate private fields according to the data stored in the DB
-	}
+	private ParseObject area;
 	
+	public ParkingArea(){
+		
+	}
+
+	public ParkingArea(StickersColor color, int areaId, int free, int taken, int total, Set<ParkingSlot> parkingSlots,
+			Set<ParkingSlot> freeSlots, Set<ParkingSlot> takenSlots) throws ParseException {
+		// will populate private fields according to the data stored in the DB
+		DBManager.initialize();
+		this.area = new ParseObject("ParkingArea");
+		this.setAreaId(areaId);
+		this.setColor(color);
+		this.setFreeSlots(freeSlots);
+		this.setNumOfFreeSlots(free);
+		this.setNumOfParkingSlots(total);
+		this.setNumOfTakenSlots(taken);
+		this.setParkingSlots(parkingSlots);
+		this.setTakenSlots(takenSlots);
+		area.save();
+	}
+
 	public int getAreaId() {
 		return areaId;
 	}
 
 	public void setAreaId(int areaId) {
 		this.areaId = areaId;
+		this.area.put("areaId", areaId);
 	}
 
 	public int getNumOfParkingSlots() {
@@ -38,53 +63,7 @@ public class ParkingArea {
 
 	public void setNumOfParkingSlots(int numOfParkingSlots) {
 		this.numOfParkingSlots = numOfParkingSlots;
-	}
-
-	public Set<ParkingSlot> getParkingSlot() {
-		return parkingSlots;
-	}
-
-	public void setParkingSlot(Set<ParkingSlot> ¢) {
-		this.parkingSlots = ¢;
-	}
-
-	public int getNumOfFreeSlots() {
-		return numOfFreeSlots;
-	}
-
-	public void setNumOfFreeSlots(int numOfFreeSlots) {
-		this.numOfFreeSlots = numOfFreeSlots;
-	}
-
-	public int getNumOfTakenSlots() {
-		return numOfTakenSlots;
-	}
-
-	public void setNumOfTakenSlots(int numOfTakenSlots) {
-		this.numOfTakenSlots = numOfTakenSlots;
-	}
-
-	public Set<ParkingSlot> getFreeSlots() {
-		return freeSlots;
-	}
-
-	public void setFreeSlots(Set<ParkingSlot> freeSlots) {
-		this.freeSlots = freeSlots;
-	}
-
-	public Set<ParkingSlot> getTakenSlots() {
-		return takenSlots;
-	}
-
-	public void setTakenSlots(Set<ParkingSlot> takenSlots) {
-		this.takenSlots = takenSlots;
-	}
-	public StickersColor getColor() {
-		return color;
-	}
-
-	public void setColor(StickersColor color) {
-		this.color = color;
+		this.area.put("numOfParkingSlots", numOfParkingSlots);
 	}
 
 	public Set<ParkingSlot> getParkingSlots() {
@@ -93,6 +72,52 @@ public class ParkingArea {
 
 	public void setParkingSlots(Set<ParkingSlot> parkingSlots) {
 		this.parkingSlots = parkingSlots;
+		this.area.put("parkingSlots", parkingSlots);
+	}
+
+	public int getNumOfFreeSlots() {
+		return numOfFreeSlots;
+	}
+
+	public void setNumOfFreeSlots(int numOfFreeSlots) {
+		this.numOfFreeSlots = numOfFreeSlots;
+		this.area.put("numOfFreeSlots", numOfFreeSlots);
+	}
+
+	public int getNumOfTakenSlots() {
+		return numOfTakenSlots;
+	}
+
+	public void setNumOfTakenSlots(int numOfTakenSlots) {
+		this.numOfTakenSlots = numOfTakenSlots;
+		this.area.put("numOfTakenSlots", numOfTakenSlots);
+	}
+
+	public Set<ParkingSlot> getFreeSlots() {
+		return freeSlots;
+	}
+
+	public void setFreeSlots(Set<ParkingSlot> freeSlots) {
+		this.freeSlots = freeSlots;
+		this.area.put("freeSlots", freeSlots);
+	}
+
+	public Set<ParkingSlot> getTakenSlots() {
+		return takenSlots;
+	}
+
+	public void setTakenSlots(Set<ParkingSlot> takenSlots) {
+		this.takenSlots = takenSlots;
+		this.area.put("takenSlots", takenSlots);
+	}
+
+	public StickersColor getColor() {
+		return color;
+	}
+
+	public void setColor(StickersColor color) {
+		this.color = color;
+		this.area.put("color", color);
 	}
 
 	/*
@@ -100,11 +125,16 @@ public class ParkingArea {
 	 * free slot, and therefore increase the amount of free slots in this area,
 	 * and the total count of parking
 	 */
-	public void addParkingSlot(ParkingSlot ¢) {
-		this.getFreeSlots().add(¢);
-		this.getParkingSlot().add(¢);
+	public void addParkingSlot(ParkingSlot slot) {
+		this.getFreeSlots().add(slot);
+		this.getParkingSlots().add(slot);
 		this.setNumOfFreeSlots(this.getNumOfFreeSlots() + 1);
 		this.setNumOfParkingSlots(this.getNumOfParkingSlots() + 1);
+
+		this.area.put("freeSlots", this.getFreeSlots());
+		this.area.put("parkingSlot", this.getParkingSlots());
+		this.area.put("numOfFreeSlots", this.getNumOfFreeSlots());
+		this.area.put("numOfParkingSlots", this.getNumOfParkingSlots());
 	}
 
 	/*
@@ -114,12 +144,35 @@ public class ParkingArea {
 		if (!this.getTakenSlots().contains(s))
 			return;
 
+		// search the parkingSlot objectId from parse
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("ParkingSlot");
+		query.whereEqualTo("name", s.getName());
+		try {
+			List<ParseObject> slotList = query.find();
+			if (slotList.size() != 1) {
+				System.out.format("Something went worng while searching for %d", s.getName());
+				return;
+			}
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return;
+		}
+
 		this.getTakenSlots().remove(s);
 		this.setNumOfTakenSlots(this.getNumOfTakenSlots() - 1);
 		s.setStatus(ParkingSlotStatus.FREE);
-//		slot.setCurrentUser(null);
+		// FIXME: where will we save the current user of a given slot?
+		// s.setCurrentUser(null);
 		this.getFreeSlots().add(s);
 		this.setNumOfFreeSlots(this.getNumOfFreeSlots() + 1);
+
+		
+		this.area.put("takenSlot", this.getTakenSlots());
+		this.area.put("numOfTakenSlots", this.getNumOfTakenSlots());
+		this.area.put("freeSlots", this.getFreeSlots());
+		this.area.put("numOfFreeSlots", this.getNumOfFreeSlots());
+		
 	}
 
 	/*
@@ -128,13 +181,50 @@ public class ParkingArea {
 	public void changeFreeToTaken(ParkingSlot s, User user) {
 		if (!this.getFreeSlots().contains(s)) {
 			return;
+		}	
+		// search the parkingSlot objectId from parse
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("ParkingSlot");
+		query.whereEqualTo("name", s.getName());
+		try {
+			List<ParseObject> slotList = query.find();
+			if (slotList.size() != 1) {
+				System.out.format("Something went worng while searching for %d", s.getName());
+				return;
+			}
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return;
 		}
+		
+		//search the given user
+		ParseQuery<ParseObject> query2 = ParseQuery.getQuery("User");
+		query.whereEqualTo("carNumber", user.getCarNumber());
+		try {
+			List<ParseObject> usersList = query2.find();
+			if (usersList.size() != 1) {
+				System.out.format("Something went worng while searching for %d", user.getCarNumber());
+				return;
+			}
+			
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return;
+		}
+		
 		this.getFreeSlots().remove(s);
 		this.setNumOfFreeSlots(this.getNumOfFreeSlots() - 1);
 		s.setStatus(ParkingSlotStatus.TAKEN);
 		user.setCurrentParking(s);
-//		slot.setCurrentUser(user);
+		// FIXME: where will we save the current user of a given slot?
+		// s.setCurrentUser(user);
 		this.getTakenSlots().add(s);
 		this.setNumOfTakenSlots(this.getNumOfTakenSlots() + 1);
+		
+		this.area.put("takenSlot", this.getTakenSlots());
+		this.area.put("numOfTakenSlots", this.getNumOfTakenSlots());
+		this.area.put("freeSlots", this.getFreeSlots());
+		this.area.put("numOfFreeSlots", this.getNumOfFreeSlots());
+		
 	}
 }
