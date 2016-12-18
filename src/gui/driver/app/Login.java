d5fae9dfa33cb79acc75dee6ff8bb335cae06c6d
Shahar-Y <shya95@gmail.com>
2016-12-18 08:49:51 +0200

    @Shahar-Y took care of the GUI warnings. sorry, my bad :(

--- src/gui/driver/app/Login.java
+++ src/gui/driver/app/Login.java
@@ -8,7 +8,6 @@ package gui.driver.app;
 import java.util.ArrayList;
 
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -20,12 +19,10 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.TextAlignment;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Login extends AbstractWindow {
