36c754dc3e614b6d94ef2219458c9de438d8fcb1
zahimizrahi <zahimizrahi1@gmail.com>
2016-12-21 20:50:42 +0200

    css classes #55

--- src/gui/driver/zahiLearning/example.java
+++ src/gui/driver/zahiLearning/example.java
@@ -33,22 +33,27 @@ public class example extends Application {
 		grid.setHgap(10);
 		
 		Label user = new Label ("Username");
+		user.setId("label-bold");
 		TextField nameInput = new TextField("Zahi");
-
 		
 		Label pass = new Label ("Password");
 		TextField passInput = new TextField();
 		passInput.setPromptText("password");
 		
-		Button button = new Button ("Login");
+		Button button1 = new Button ("Login");
+		
+		Button button2 = new Button ("Sign Up"); 
+		button2.getStyleClass().add("button-blue");
+		
 		GridPane.setConstraints (user,0,0);
 		GridPane.setConstraints(nameInput, 1,0); 
 		GridPane.setConstraints(pass, 0,1);
 		GridPane.setConstraints(passInput, 1,1);
-		GridPane.setConstraints(button,1,2); 
+		GridPane.setConstraints(button1,1,2); 
+		GridPane.setConstraints(button2,1,3); 
 		
 		
-		grid.getChildren().addAll(user,nameInput,pass,passInput,button);
+		grid.getChildren().addAll(user,nameInput,pass,passInput,button1, button2);
 		Scene scene = new Scene (grid);
 		scene.getStylesheets().add(getClass().getResource("example_style.css").toExternalForm());
 		window.setScene(scene);
