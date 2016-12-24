0841ba0a72a03d6afb46160ab87530a826a745d8
zahimizrahi <zahimizrahi1@gmail.com>
2016-12-24 12:39:59 +0200

    upload fxml file to project #55

--- src/data/members/Faculty.java
+++ src/data/members/Faculty.java
@@ -33,8 +33,8 @@ public class Faculty {
 		this.entrance = new MapLocation(latitude, longitude);
 	}
 
-	public void setEntrance(MapLocation location) {
-		this.entrance = location;
+	public void setEntrance(MapLocation ¢) {
+		this.entrance = ¢;
 	}
 
 }
