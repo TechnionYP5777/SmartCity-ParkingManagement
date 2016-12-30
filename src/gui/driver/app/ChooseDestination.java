9f41199514d80e9d75a2f248d2d6e5f8ce8bfab0
zahimizrahi <zahimizrahi1@gmail.com>
2016-12-30 15:48:06 +0200

    added to chooseDestination #85

--- src/gui/driver/app/ChooseDestination.java
+++ src/gui/driver/app/ChooseDestination.java
@@ -1,6 +1,5 @@
 package gui.driver.app;
 
-import java.awt.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 import javafx.geometry.Insets;
@@ -8,11 +7,15 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class ChooseDestination extends AbstractWindow{
@@ -24,7 +27,16 @@ public class ChooseDestination extends AbstractWindow{
 	
 	public void display(Stage primaryStage){
 		
-		window.setTitle("My Details");
+		window.setTitle("Choose Destination");
+		Label title = new Label(); 
+		DropShadow shadow = new DropShadow();
+		shadow.setOffsetY(4.0);
+		shadow.setColor(Color.color(0.4f, 0.4f, 0.4f));
+		title.setEffect(shadow);
+		title.setTextAlignment(TextAlignment.CENTER);
+		title.setText("Navigate");
+		title.setFont(Font.font(null, FontWeight.BOLD, 48));
+		title.getStyleClass().add("label-title"); 
 		GridPane grid = new GridPane();
 		grid.setPadding(new Insets(20, 20, 20, 20));
 		grid.setVgap(15);
@@ -33,17 +45,19 @@ public class ChooseDestination extends AbstractWindow{
 				new Background(new BackgroundFill(Color.LIGHTBLUE, CornerRadii.EMPTY,
 						new Insets(2, 2, 2, 2))));
 
-		List list = new List();
+	//	List list = new List();
 	
 		ArrayList<String> locationsList = new ArrayList<String>(Arrays.asList(
 				"Shaar Nesher", "Shaar Nosh", "Taub", "Fishbah",
 				"Rifkin Dorms", "Canada Dorms"));
 		
-		int currIdx = 0;
+		GridPane.setConstraints(title, 1, 0); 
+		int currIdx = 1; 
 		Label from = new Label("From:");
 		ChoiceBox<String> fromValue = new ChoiceBox<>();
 		fromValue.getItems().addAll(locationsList);
 		fromValue.setValue("Shaar Nesher");
+		fromValue.getStyleClass().add("cb");
 		GridPane.setConstraints(fromValue, 1, currIdx);
 		GridPane.setConstraints(from, 0, currIdx++);
 		
@@ -51,6 +65,7 @@ public class ChooseDestination extends AbstractWindow{
 		ChoiceBox<String> toValue = new ChoiceBox<>();
 		toValue.getItems().addAll(locationsList);
 		toValue.setValue("Shaar Nosh");
+		toValue.getStyleClass().add("cb");
 		GridPane.setConstraints(toValue, 1, currIdx);
 		GridPane.setConstraints(to, 0, currIdx++);
 		
@@ -70,11 +85,8 @@ public class ChooseDestination extends AbstractWindow{
 		Button buttonGO = new Button("GO!");
 		buttonGO.setPrefSize(100, 100);
 		//buttonGO.setStyle("-fx-background-color: green; -fx-text-fill: black;");
-		buttonGO.setStyle("-fx-background-color: #3c7fb1, linear-gradient(#fafdfe, #e8f5fc), "
-				+ "linear-gradient(#eaf6fd 0%, #d9f0fc 49%, #bee6fd 50%, #a7d9f5 100%);"
-				+ "-fx-background-insets: 0,1,2; -fx-background-radius: 3,2,1; -fx-padding: 3 30 3 30;"
-				+ "-fx-text-fill: black; -fx-font-size: 14px;");
-		//buttonGO.setHover(true);
+		
+		buttonGO.getStyleClass().add("button-go");
 		
 		buttonGO.setOnAction(e -> {
 			//************Add navigation functionality************//
@@ -86,9 +98,10 @@ public class ChooseDestination extends AbstractWindow{
 		GridPane.setConstraints(buttonGO, 1, currIdx++);
 		//buttonGO.setDisable(true);
 		
-		grid.getChildren().addAll(from, fromValue, to, toValue, buttonBack, buttonGO);
+		grid.getChildren().addAll(title, from, fromValue, to, toValue, buttonBack, buttonGO);
 		
-		Scene scene = new Scene(grid, 300, 150);
+		Scene scene = new Scene(grid, 400, 250);
+		scene.getStylesheets().add(getClass().getResource("mainStyle.css").toExternalForm());
 		window.setScene(scene);
 		window.show();
 	}
