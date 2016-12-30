eab0580a6107b1fd3ff9d8ccea030de74374b030
dshames <dani.shames@gmail.com>
2016-12-30 09:59:04 -0800

    updated the functions according to db #82

--- src/data/members/Destination.java
+++ src/data/members/Destination.java
@@ -1,20 +1,41 @@
 package data.members;
 
+import org.parse4j.ParseException;
+import org.parse4j.ParseGeoPoint;
+
+import data.management.DBManager;
+
 /**
+ * @author dshames
  * @Author DavidCohen55
  */
-public class Destination {
-	private MapLocation entrance;
+public class Destination extends dbMember {
+	
+	// destination name, for example: Pool, Taub, etc..
 	private String name;
+	
+	// destination's entrance geo location
+	private MapLocation entrance;
+	
+	private static final String NAME = "name";
+	private static final String ENTRANCE = "entrance";
+	private static final String TABLE_NAME = "Destination";
 
-	public Destination(String name, double latitude, double longitude) {
-		this.entrance = new MapLocation(latitude, longitude);
-		this.name = name;
+	public Destination(String name, MapLocation location) throws ParseException {
+		
+		DBManager.initialize();
+		this.setParseObject(TABLE_NAME);
+		this.setEntrance(location);
+		this.setDestinationName(name);
+		this.setObjectId();
 	}
-
-	public Destination(String name, MapLocation location) {
-		this.entrance = location;
-		this.name = name;
+	
+	public Destination(String name, double latitude, double longitude) throws ParseException {
+		DBManager.initialize();
+		this.setParseObject(TABLE_NAME);
+		this.setEntrance(new MapLocation(latitude, longitude));
+		this.setDestinationName(name);
+		this.setObjectId();
 	}
 
 	public String getDestinationName() {
@@ -25,16 +46,22 @@ public class Destination {
 		return this.entrance;
 	}
 
-	public void setDestinationName(String name) {
+	public void setDestinationName(String name) throws ParseException {
+		
 		this.name = name;
+		this.parseObject.put(NAME, name);
+		this.parseObject.save();
 	}
-
-	public void setEntrance(double latitude, double longitude) {
-		this.entrance = new MapLocation(latitude, longitude);
-	}
-
-	public void setEntrance(MapLocation ¢) {
+	public void setEntrance(MapLocation ¢) throws ParseException {
 		this.entrance = ¢;
+		this.parseObject.put(ENTRANCE, new ParseGeoPoint(¢.getLat(), ¢.getLon()));
+		this.parseObject.save();
+		
 	}
+
+	public void setEntrance(double latitude, double longitude) throws ParseException {
+		setEntrance(new MapLocation(latitude, longitude));
+	}
+
 
 }
