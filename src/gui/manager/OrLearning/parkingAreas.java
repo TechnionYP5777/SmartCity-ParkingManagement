4349bf55a8b85efdac56784d9003ac6d0120d96a
Or Troyaner <or.troyaner@gmail.com>
2017-01-01 17:31:49 +0200

    Table View learning #61

---
+++ src/gui/manager/OrLearning/parkingAreas.java
@@ -0,0 +1,44 @@
+package gui.manager.OrLearning;
+
+import gui.manager.OrLearning.CreatingABasicWindow.Color;
+
+public class parkingAreas {
+	private String name;
+	private Color color;
+	private int spots;
+	
+	//Constructors
+	parkingAreas() {
+		this.name="";
+		this.color=Color.GRAY;
+		this.spots=0;
+	}
+	
+	parkingAreas(String name, Color color, int spots) {
+		this.name=name;
+		this.color=color;
+		this.spots=spots;
+	}
+	
+	//Getters & Setters
+	public String getName() {
+		return this.name;
+	}
+	void setName(String name) {
+		this.name=name;
+	}
+	
+	public Color getColor() {
+		return this.color;
+	}
+	void setColor(Color ¢) {
+		this.color=¢;
+	}
+	
+	public int getSpots() {
+		return this.spots;
+	}
+	void setSpots(int spots) {
+		this.spots=spots;
+	}
+}
