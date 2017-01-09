eb3548e87fab19a69e747f8fdab8d4013cc84154
Or Troyaner <or.troyaner@gmail.com>
2017-01-09 16:17:47 +0200

    1st version of gui manager sekelton #46

---
+++ src/gui/manager/app/Main.java
@@ -0,0 +1,22 @@
+package gui.manager.app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("ManagerMainScreen.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 500, 275));
+        primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/gui/manager/app/ManagerMainScreenContorller.java
@@ -0,0 +1,14 @@
+package gui.manager.app;
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ManagerMainScreenContorller implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle __) {
+        System.out.println("View is now loaded!");
+    }
+
+}
