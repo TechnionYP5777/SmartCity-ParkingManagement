ddfe03f1e924595306123040dab06e53fdc8e20d
shaysegal <siglons@gmail.com>
2017-01-13 15:49:39 +0200

    add marker of parking area on ManagerMap #105 @shaysegal

--- src/gui/map/ManegerMap.java
+++ src/gui/map/ManegerMap.java
@@ -1,4 +1,9 @@
 package gui.map;
+
+import com.lynden.gmapsfx.javascript.object.LatLong;
+import com.lynden.gmapsfx.shapes.Circle;
+import com.lynden.gmapsfx.shapes.CircleOptions;
+
 /*
  * @Author shay segal
  */
@@ -6,6 +11,9 @@ public class ManegerMap extends PmMap {
 	@Override
 	public void mapInitialized() {
 		super.mapInitialized();
-		
+		CircleOptions c_ops=new CircleOptions().center(new LatLong(32.777469, 35.021177)).fillColor("red").radius(20.0).fillOpacity(0.40);
+		Circle c =new Circle(c_ops);
+		map.addMapShape(c);
+		scene.getWindow().sizeToScene();
 	}
 }
