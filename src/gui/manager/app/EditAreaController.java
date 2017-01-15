7e6cc555163a68a951d2ceb0898cdb07ab1b86c9
Or Troyaner <or.troyaner@gmail.com>
2017-01-16 00:51:49 +0200

    Build a scene for editing area #111 #93

--- src/gui/manager/app/EditAreaController.java
+++ src/gui/manager/app/EditAreaController.java
@@ -1,5 +1,28 @@
 package gui.manager.app;
 
-public class EditAreaController {
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXSlider;
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.JFXToggleButton;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 
+public class EditAreaController {
+    @FXML
+    private AnchorPane curbstoneView;
+
+    @FXML
+    private JFXSlider slotsSlider;
+
+    @FXML
+    private JFXTextField slotsInput;
+
+    @FXML
+    private JFXToggleButton tempToggle;
+
+    @FXML
+    private JFXButton cancelBtn;
+
+    @FXML
+    private JFXButton ChngBtn;
 }
