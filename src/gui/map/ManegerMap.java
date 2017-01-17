f2fce483ee27aa9d43791097858ead3a971f1971
shaysegal <siglons@gmail.com>
2017-01-17 13:43:42 +0200

    drop stubs from manager map

--- src/gui/map/ManegerMap.java
+++ src/gui/map/ManegerMap.java
@@ -1,12 +1,10 @@
 package gui.map;
 
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Random;
+
 
 import com.lynden.gmapsfx.GoogleMapView;
-import com.lynden.gmapsfx.javascript.event.UIEventType;
 import com.lynden.gmapsfx.javascript.object.LatLong;
 import com.lynden.gmapsfx.javascript.object.LatLongBounds;
 import com.lynden.gmapsfx.javascript.object.MapOptions;
@@ -53,65 +51,16 @@ public class ManegerMap extends PmMap {
 				shapes.put(key, c);
 				map.addMapShape(c);
 			}
-			if(parkingToLocation.isEmpty()){
-			Circle taub=new Circle(new CircleOptions().center(new LatLong(32.777469, 35.021177)).fillColor("red")
-					.radius(20.0).fillOpacity(0.40).editable(false).clickable(true));
-			map.addMapShape(taub);
-			Circle StudentHous=new Circle(new CircleOptions().center(new LatLong(32.776009, 35.022816)).fillColor("blue")
-					.radius(20.0).fillOpacity(0.40).editable(false).clickable(true));
-			map.addMapShape(StudentHous);
-			Circle pool=new Circle(new CircleOptions().center(new LatLong(32.779639, 35.019509)).fillColor("green")
-					.radius(20.0).fillOpacity(0.40).editable(false).clickable(true));
-			map.addMapShape(pool);
-			map.addMapShape((new Circle(new CircleOptions().center(new LatLong(32.780694, 35.022191)).fillColor("blue")
-					.radius(20.0).fillOpacity(0.40).editable(false).clickable(true))));
-			}
 		});
-		ArrayList<String> debug = new ArrayList<String>();
-		map.addUIEventHandler(UIEventType.click, e -> {
-			debug.add("taub");
-			debug.add("studentHous");
-			debug.add("pool");
-			debug.add("makak");
-			focusOnParkingArea(debug.get((new Random()).nextInt(debug.size() - 1)));
-		});
-	
-
 	}
     public void SetMapComponent(GoogleMapView mapComponent){
 		mapComponent.addMapInializedListener(this);
 		this.mapComponent=mapComponent;
 	}
     public void focusOnParkingArea(String AreaID){    	
-    	if (!parkingToLocation.isEmpty()) {
 			MapLocation ml = parkingToLocation.get(AreaID);
 			map.setCenter(new LatLong(ml.getLat(), ml.getLon()));
 			map.setZoom(17);
-		} else
-			switch (AreaID) {
-			case "taub":
-				map.setCenter(new LatLong(32.777469, 35.021177));
-				map.setZoom(17);
-				System.out.println("taub");
-				break;
-			case "studentHous":
-				System.out.println("StudentHous");
-				map.setCenter(new LatLong(32.776009, 35.022816));
-				map.setZoom(17);
-				break;
-			case "pool":
-				map.setCenter(new LatLong(32.779639, 35.019509));
-				System.out.println("pool");
-				map.setZoom(17);
-				break;
-			case "makak":
-				map.setCenter(new LatLong(32.780694, 35.022191));
-				System.out.println("makak");
-				map.setZoom(17);
-				break;
-			default:
-				System.out.println("dont have this label");
-			}
     }
     public void ChangeColorOfParking(String id ,String color){
     	parkingToColor.put(id, color);
