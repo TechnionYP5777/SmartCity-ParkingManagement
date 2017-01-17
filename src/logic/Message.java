8baf6782958873450500d4726f7d7c61c0756b56
sefialbo <sefialbo@gmail.com>
2017-01-17 02:27:39 +0200

    improve message class #40

--- src/logic/Message.java
+++ src/logic/Message.java
@@ -4,25 +4,19 @@ import java.io.IOException;
 
 public class Message {
 	
-  public static void main(String[] args) throws IOException {
-    Email from = new Email("SmartParking@emailservice.com");
-    String subject = "Hello World from the SendGrid Java Library!";
-    Email to = new Email(""); // put your email in here
-    Content content = new Content("text/plain", "Hello, Email!");
-    Mail mail = new Mail(from, subject, to, content);
-    SendGrid sg = new SendGrid("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");
-    Request request = new Request();
-    try {
-      request.method = Method.POST;
-      request.endpoint = "mail/send";
-      request.body = mail.build();
-      Response response = sg.api(request);
-      System.out.println(response.statusCode);
-      System.out.println(response.body);
-      System.out.println(response.headers);
-    } catch (IOException ex) {
-      throw ex;
-    }
-  }
+  private static final String EMAIL = "SmartParking@emailservice.com";
   
+  private static final String API_KEY = "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";
+  	
+  public static void sendEmail(String userEmail, String subject, String message) throws IOException {
+    Email from = new Email(EMAIL);
+    Email to = new Email(userEmail);
+    Content content = new Content("text/plain", message);
+    Mail mail = new Mail(from, subject, to, content);
+    Request request = new Request();
+    request.method = Method.POST;
+    request.endpoint = "mail/send";
+    request.body = mail.build();
+    new SendGrid(API_KEY).api(request);
+  }
 }
