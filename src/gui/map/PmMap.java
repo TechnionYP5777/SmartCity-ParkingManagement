c6bcf816714abd6defd4c36a071e95a825dde7ff
zahimizrahi <zahimizrahi1@gmail.com>
2017-01-23 10:52:34 +0200

    fixed 2 warnings

--- src/gui/map/PmMap.java
+++ src/gui/map/PmMap.java
@@ -32,8 +32,6 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ToolBar;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
