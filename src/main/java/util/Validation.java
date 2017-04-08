49fd1d22d0005c0238b760a6e2877e56a5973c62
Or Troyaner <or.troyaner@gmail.com>
2017-04-09 01:12:15 +0300

    Added basic validation function, tests and gui test. issue: #144

--- src/main/java/util/Validation.java
+++ src/main/java/util/Validation.java
@@ -1,12 +1,22 @@
 package util;
 
+import java.util.regex.Pattern;
+
 public class Validation {
 	public static boolean isInt(String input) throws Exception {
 		try {
 			Integer.parseInt(input);
 			return true;
 		} catch(NumberFormatException e) {
-			throw new Exception("Error: " + input +"is not an Integer");
+			return false;
 		}
+	}
+	
+	public static boolean isFullNamePattern(String input) {
+		return Pattern.compile("^[\\p{L} .'-]+$", Pattern.CASE_INSENSITIVE).matcher(input).find();
+	}
+	
+	public static boolean isLicensePlatePattern(String input) {
+		return Pattern.compile("^[0-9]{6,7}$", Pattern.CASE_INSENSITIVE).matcher(input).find();
 	}
 }
--- src/test/java/util/validationTest.java
+++ src/test/java/util/validationTest.java
@@ -1,28 +1,28 @@
 package util;
 
-import static org.junit.Assert.fail;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class validationTest {
 	@Test
-	public void testIsInt1() {
-		String textString = "textNotInt";
-		try {
-			Validation.isInt(textString);
-			fail();
-		} catch (Exception e) {
-			return;
-		}
+	public void testIsInt() throws Exception {
+		Assert.assertEquals(false, Validation.isInt("textNotInt"));
+		Assert.assertEquals(true, Validation.isInt("123456789"));
 	}
 	
 	@Test
-	public void testIsInt2() {
-		String intString = "123456789";
-		try {
-			Validation.isInt(intString);
-		} catch (Exception e) {
-			fail();
-		}
+	public void testIsFullNamePattern() {
+		Assert.assertEquals(true, Validation.isFullNamePattern("Gadi Eli"));
+		Assert.assertEquals(true, Validation.isFullNamePattern("Gadi Ben-Ari"));
+		Assert.assertEquals(false, Validation.isFullNamePattern("Gadi Eli2"));
+		Assert.assertEquals(false, Validation.isFullNamePattern("Gadi#Eli"));
+	}
+	
+	@Test
+	public void testIsLicensePlatePattern() {
+		Assert.assertEquals(true, Validation.isLicensePlatePattern("1122211"));
+		Assert.assertEquals(true, Validation.isLicensePlatePattern("111222"));
+		Assert.assertEquals(false, Validation.isLicensePlatePattern("1122"));
+		Assert.assertEquals(false, Validation.isLicensePlatePattern("aPlateNmber"));
 	}
 }
