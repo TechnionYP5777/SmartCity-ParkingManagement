dd5990781fc7a725646fb44120a0aeb581d6f1cc
assaflu <assaf.ss3@gmail.com>
2017-05-18 13:16:05 +0300

    create and interface and class that warrp DBManager #163

---
+++ src/main/java/data/management/DatabaseComunicationImpl.java
@@ -0,0 +1,68 @@
+package data.management;
+
+import java.util.List;
+import java.util.Map;
+
+import org.parse4j.ParseObject;
+import org.parse4j.callback.DeleteCallback;
+import org.parse4j.callback.GetCallback;
+import org.parse4j.callback.SaveCallback;
+
+import Exceptions.LoginException;
+
+public class DatabaseComunicationImpl implements DatabaseManager {
+
+	@Override
+	public void insertObject(String objectClass, Map<String, Object> keyValues, Map<String, Object> fields) {
+			DBManager.insertObject(objectClass, keyValues, fields);
+	}
+
+	@Override
+	public void insertObject(String objectClass, Map<String, Object> keyValues, Map<String, Object> fields,
+			SaveCallback c) {	
+		DBManager.insertObject(objectClass, keyValues, fields, c);
+	}
+
+	@Override
+	public void deleteObject(String objectClass, Map<String, Object> fields) {
+		DBManager.deleteObject(objectClass, fields);
+	}
+
+	@Override
+	public void deleteObject(String objectClass, Map<String, Object> fields, DeleteCallback c) {
+		DBManager.deleteObject(objectClass, fields, c);		
+	}
+
+	@Override
+	public void update(String objectClass, Map<String, Object> keys, Map<String, Object> newFields) {
+		DBManager.update(objectClass, keys, newFields);
+	}
+
+	@Override
+	public void getObjectByFields(String objectClass, Map<String, Object> values, GetCallback<ParseObject> o) {
+		DBManager.getObjectByFields(objectClass, values, o);
+	}
+
+	@Override
+	public Map<String, Object> getObjectFieldsByKey(String objectClass, Map<String, Object> keys) {
+		return DBManager.getObjectFieldsByKey(objectClass, keys);
+	}
+
+	@Override
+	public List<ParseObject> getAllObjects(String objectClass, int startLimit) {
+		return getAllObjects(objectClass, startLimit);
+	}
+
+	@Override
+	public void login(String userClass, String userNameKey, String userName, String passwordKey, String password)
+			throws LoginException {
+		DBManager.login(userClass, userNameKey, userName, passwordKey, password);		
+	}
+
+	@Override
+	public void register(String userClass, Map<String, Object> userKeys, Map<String, Object> userFields)
+			throws LoginException {
+		DBManager.register(userClass, userKeys, userFields);
+	}
+
+}
---
+++ src/main/java/data/management/DatabaseManager.java
@@ -0,0 +1,34 @@
+package data.management;
+
+import java.util.List;
+import java.util.Map;
+
+import org.parse4j.ParseObject;
+import org.parse4j.callback.DeleteCallback;
+import org.parse4j.callback.GetCallback;
+import org.parse4j.callback.SaveCallback;
+
+import Exceptions.LoginException;
+
+public interface DatabaseManager {
+
+	public void insertObject(final String objectClass, Map<String, Object> keyValues,Map<String, Object> fields);
+	
+	public void insertObject(final String objectClass,Map<String, Object> keyValues, Map<String, Object> fields,SaveCallback c);
+
+	public void deleteObject(final String objectClass,Map<String, Object> fields);
+	
+	public void deleteObject(final String objectClass,Map<String, Object> fields,DeleteCallback c);
+	
+	public void update(final String objectClass,Map<String, Object> keys,Map<String, Object> newFields);
+	
+	public void getObjectByFields(final String objectClass, Map<String, Object> values,GetCallback<ParseObject> o);
+	
+	public Map<String, Object> getObjectFieldsByKey(final String objectClass, Map<String, Object> keys);
+	
+	public List<ParseObject> getAllObjects (final String objectClass,int startLimit);
+	
+	public void login (String userClass,String userNameKey, String userName, String passwordKey, String password) throws LoginException;
+	
+	public void register(String userClass, Map<String, Object> userKeys, Map<String, Object> userFields)throws LoginException;
+}
