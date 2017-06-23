ed873d4136d6774f0698d9b1105996e7bb2276d6
dshames <dani.shames@gmail.com>
2017-06-23 09:36:49 +0300

    deleted destination marker and redirection to main screen after making order #172

--- src/main/java/gui/driver/app/ChooseParkingSlotController.java
+++ src/main/java/gui/driver/app/ChooseParkingSlotController.java
@@ -277,7 +277,7 @@ public class ChooseParkingSlotController {
 					if(sportsCenterRadioButton.isSelected()){
 		   				location = "32.779119, 35.019112";
 		   			}
-	       			engine.executeScript("addDestinationMarker(" + location + ");");
+	       			//engine.executeScript("addDestinationMarker(" + location + ");");
 	       			
 	       			if (result.size() != 0){
 	       				slotsTable.getSelectionModel().selectFirst();
@@ -322,6 +322,18 @@ public class ChooseParkingSlotController {
 	            	   System.out.println("SUCCESS");
 	               } else {
 	            	   System.out.println("FAILURE");
+	               }
+	               try{
+		               Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		               window.setTitle("Main Screen");
+		               FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainScreen.fxml"));     
+		               Parent root = (Parent)fxmlLoader.load();          
+		               MainScreenController controller = fxmlLoader.<MainScreenController>getController();
+		               controller.setUserId(userId);
+		               window.setScene(new Scene(root,750,650));		
+		               window.show();
+	               } catch(Exception e){
+	            	   
 	               }
 	   			}
 
