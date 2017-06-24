6d05b536a84a55d0a625ced0ef690da790126634
dshames <dani.shames@gmail.com>
2017-06-24 23:26:26 +0300

    sending email notifications on forgot password and registration and work on controllers #172 #183

--- src/main/java/gui/driver/app/ChooseParkingSlotController.java
+++ src/main/java/gui/driver/app/ChooseParkingSlotController.java
@@ -313,6 +313,8 @@ public class ChooseParkingSlotController {
 	       progressIndicator.progressProperty().bind(orderTask.progressProperty());
 	       progressIndicator.setVisible(true); 
 	       
+	       // TODO: parking slot catched
+	       
 	       orderTask.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
 	           @Override
 	           public void handle(WorkerStateEvent workerStateEvent) {
@@ -320,21 +322,21 @@ public class ChooseParkingSlotController {
 	               boolean result =  orderTask.getValue();
 	               if(result){
 	            	   System.out.println("SUCCESS");
-	               } else {
-	            	   System.out.println("FAILURE");
-	               }
-	               try{
-		               Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
-		               window.setTitle("Main Screen");
-		               FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainScreen.fxml"));     
-		               Parent root = (Parent)fxmlLoader.load();          
-		               MainScreenController controller = fxmlLoader.<MainScreenController>getController();
-		               controller.setUserId(userId);
-		               window.setScene(new Scene(root,750,650));		
-		               window.show();
-	               } catch(Exception e){
 	            	   
+		               try{
+			               Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+			               window.setTitle("Main Screen");
+			               FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainScreen.fxml"));     
+			               Parent root = (Parent)fxmlLoader.load();          
+			               MainScreenController controller = fxmlLoader.<MainScreenController>getController();
+			               controller.setUserId(userId);
+			               window.setScene(new Scene(root,750,650));		
+			               window.show();
+		               } catch(Exception e){
+		            	   
+		               }
 	               }
+
 	   			}
 
 	       });
