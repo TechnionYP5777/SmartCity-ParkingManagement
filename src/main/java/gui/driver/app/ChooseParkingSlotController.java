edbfbbb296a81a098e0660ba463f1b861e0f8eca
dshames <dani.shames@gmail.com>
2017-06-26 20:21:03 +0300

    notify user about errors #172

--- src/main/java/gui/driver/app/ChooseParkingSlotController.java
+++ src/main/java/gui/driver/app/ChooseParkingSlotController.java
@@ -47,18 +47,7 @@ public class ChooseParkingSlotController {
 	 * @author dshames
 	 *  This class contains the controller of the choose parking slot screen
 	 */
-	
-	public ObservableList<PresentParkingSlot> getSlots(List<PresentParkingSlot> slots){
 		
-		ObservableList<PresentParkingSlot> returnSlots = FXCollections.observableArrayList();
-		int index = 0;
-		for (PresentParkingSlot slot : slots){
-
-			returnSlots.add(slot);
-		}
-		return returnSlots;
-	}
-	
 	@FXML
 	private WebView myWebView;
 	@FXML
@@ -74,7 +63,6 @@ public class ChooseParkingSlotController {
 	private TableColumn<PresentParkingSlot, Double> distanceColumn;
 	@FXML
 	private TableColumn<PresentParkingSlot, Double> ratingColumn;
-	
 	@FXML
 	private Button continueButton;
 	@FXML
@@ -105,133 +93,59 @@ public class ChooseParkingSlotController {
 	private Button refreshButton;
 	@FXML
 	private Button showChartButton;
+	@FXML
+	private Label statusLabel;
 	
 	private ParkingSlotRequest request;
 	private String userId;
+	private int errorType;
+	private boolean wasShown;
 	
-	public void setUserId(String id){
-		this.userId = id;
-	}
 	
 	@FXML
     protected void initialize(){
 		progressIndicator.setVisible(false);
 		studentHouseRadioButton.setSelected(true);
-		
-		engine = myWebView.getEngine();
-		URL url = getClass().getResource("map.html");
-		engine.load(url.toExternalForm());
-		
-
-		myWebView.getEngine().setOnAlert((WebEvent<String> wEvent) -> {
-			String clickedName = wEvent.getData();
-			int index = 0;
-			for(PresentParkingSlot parkingSlot :slotsTable.getItems()){
-				if (parkingSlot.getName().equals(clickedName)){
-					slotsTable.getSelectionModel().select(index);
-					slotsTable.getSelectionModel().focus(index);
-					break;
-				}
-				index++;	
-			}
-		});
-		
-		idColumn = new TableColumn<>("id");
-		idColumn.setPrefWidth(130);
-		idColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-		
-		priceColumn = new TableColumn<>("price");
-		priceColumn.setPrefWidth(90);
-		priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
-		
-		distanceColumn = new TableColumn<>("distance");
-		distanceColumn.setPrefWidth(90);
-		distanceColumn.setCellValueFactory(new PropertyValueFactory<>("distance"));
-		
-		ratingColumn = new TableColumn<>("rating");
-		ratingColumn.setPrefWidth(90);
-		ratingColumn.setCellValueFactory(new PropertyValueFactory<>("rating"));
-		
-		
-       slotsTable.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
-    	   
-            if (slotsTable.getSelectionModel().getSelectedItem() != null) {
-            	if(oldValue != null){ 
-            		engine.executeScript("setUnSelected('" + oldValue.getName() + "');");	
-            	}
-            	engine.executeScript("setSelected('" + newValue.getName()  + "');");	
-            	engine.executeScript("refresh()");
-            }
-        });
-       
+		statusLabel.setVisible(false);
+		setWebViewAndTable();
        request = null;
+       wasShown = false;
     }
-	
-	private int roundMinutes(int minutes, boolean roundUp){
-		if (0 < minutes && minutes < 15){
-			if(roundUp)
-				return 15;
-			return 0;
-		}
-		if (15 < minutes && minutes < 30){
-			if(roundUp)
-				return 30;
-			return 15;
-		}
-		if (30 < minutes && minutes < 45){
-			if(roundUp)
-				return 45;
-			return 30;
-		}
-		if (45 < minutes && minutes < 60){
-			if(roundUp)
-				return 60;
-			return 45;
-		}
-		return minutes;
-	}
 	
 	
 	@FXML
 	public void continueButtonClicked(ActionEvent event) throws Exception{
-		
+			
 	       Task<List<PresentParkingSlot>> slotsTask = new Task<List<PresentParkingSlot>>() {
 	            @Override
 	            protected List<PresentParkingSlot> call() throws Exception {
 	            	
+	            	// TODO: clean map!
 	            	
-	            	// TODO: CHANGE 
-	            	
-	            	
-		        	ParseGeoPoint point = new ParseGeoPoint(32.777566, 35.022484);
+		        	ParseGeoPoint point = getSelectedDestination();
 		        	DatabaseManager d = DatabaseManagerImpl.getInstance();
 		        	d.initialize();
 		        	
 		        	LocalDate date = datePicker.getValue();
 		        	LocalTime arrivalTime = arrivalTimePicker.getValue();
 		        	LocalTime departureTime = departureTimePicker.getValue();
-
-		        	// TODO: clean map!
 		        	
+		        	errorType = -1;
 		        	if (date == null || arrivalTime == null || departureTime == null){
-		        		System.out.println("You have to fill all the date and time fields");
+		        		errorType = 0;
 		        		return null;
 		        	}
+		        	
 		        	Calendar arrivalDateTime = Calendar.getInstance();
 		        	arrivalDateTime.set(Calendar.YEAR, date.getYear());
 		        	arrivalDateTime.set(Calendar.MONTH, date.getMonthValue()-1);
-		        	//System.out.println("the month given to the calendar is: " + date.getMonthValue());
 		        	arrivalDateTime.set(Calendar.DAY_OF_MONTH,date.getDayOfMonth());
 		        	arrivalDateTime.set(Calendar.HOUR_OF_DAY, arrivalTime.getHour());
 		        	arrivalDateTime.set(Calendar.MINUTE, roundMinutes(arrivalTime.getMinute(), false));
-		        	//System.out.println("after defining arrivalDateTime, the date is: " + arrivalDateTime.toString());
-		        	/*
-		        	if (new Date(date.getYear(),date.getMonthValue(),date.getDayOfMonth(),
-		        			arrivalTime.getHour(), arrivalTime.getMinute()).before(new Date().setMinutes(-10))){
-		        		// TODO: notify user.
-		        	}*/
+		        	arrivalDateTime.set(Calendar.SECOND, 0);
+
 		        	if (!arrivalTime.isBefore(departureTime)){
-		        		System.out.println("Your departure time must be after your arrival time");
+		        		errorType = 1;
 		        		return null;
 		        	}
 		        	
@@ -242,9 +156,9 @@ public class ChooseParkingSlotController {
 		        	} else {
 		        		quartersCounter = departureTime.getHour()*4 + departureMinutes/15;
 		        	}
-		        	int diff = quartersCounter - (arrivalDateTime.get(Calendar.HOUR)*4 + arrivalDateTime.get(Calendar.MINUTE)/15);
-		        	//System.out.print("ChooseParkingSlotScreen date is: " + arrivalDateTime.getTime().toString());
+		        	int diff = quartersCounter - (arrivalDateTime.get(Calendar.HOUR_OF_DAY)*4 + arrivalDateTime.get(Calendar.MINUTE)/15);
 		        	request = new ParkingSlotRequest(point, arrivalDateTime.getTime(), diff, d);
+		        	
 		        	return request.getAllAvailableParkingSlot(new Billing() {
 						@Override
 						public double calculateCost(StickersColor rank, double distance) {
@@ -270,8 +184,16 @@ public class ChooseParkingSlotController {
 	               	List<PresentParkingSlot> result = slotsTask.getValue();   //here you get the return value of your service
 	               	if (result == null){
 	               		progressIndicator.setVisible(false);
+	               		
+	               		if (errorType == 0){
+	               			statusLabel.setText("You have to fill all the date and time fields");
+	               		} else if (errorType == 1){
+	               			statusLabel.setText("Departure time must be after your arrival time");
+	               		}
+	               		statusLabel.setVisible(true);
 	               		return;
 	               	}
+	               	statusLabel.setVisible(false);
 	               	slotsTable.setItems(getSlots(result));
 	       			slotsTable.getColumns().setAll(idColumn, priceColumn, distanceColumn, ratingColumn);
 	       			for (PresentParkingSlot slot : result){
@@ -291,7 +213,7 @@ public class ChooseParkingSlotController {
 					if(sportsCenterRadioButton.isSelected()){
 		   				location = "32.779119, 35.019112";
 		   			}
-	       			//engine.executeScript("addDestinationMarker(" + location + ");");
+	       			engine.executeScript("map.setCenter(" + location + ");");
 	       			engine.executeScript("firstRefresh();");
 	       			
 	       			if (result.size() != 0){
@@ -412,6 +334,104 @@ public class ChooseParkingSlotController {
 		// GraphPresent.PresentGraph(new ParseGeoPoint(32.777566, 35.022484));
 	}
 	
+	private void setWebViewAndTable(){
+		
+		engine = myWebView.getEngine();
+		URL url = getClass().getResource("map.html");
+		engine.load(url.toExternalForm());
+		
+
+		myWebView.getEngine().setOnAlert((WebEvent<String> wEvent) -> {
+			String clickedName = wEvent.getData();
+			int index = 0;
+			for(PresentParkingSlot parkingSlot :slotsTable.getItems()){
+				if (parkingSlot.getName().equals(clickedName)){
+					slotsTable.getSelectionModel().select(index);
+					slotsTable.getSelectionModel().focus(index);
+					break;
+				}
+				index++;	
+			}
+		});
+		
+		idColumn = new TableColumn<>("id");
+		idColumn.setPrefWidth(130);
+		idColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+		
+		priceColumn = new TableColumn<>("price");
+		priceColumn.setPrefWidth(90);
+		priceColumn.setCellValueFactory(new PropertyValueFactory<>("price"));
+		
+		distanceColumn = new TableColumn<>("distance");
+		distanceColumn.setPrefWidth(90);
+		distanceColumn.setCellValueFactory(new PropertyValueFactory<>("distance"));
+		
+		ratingColumn = new TableColumn<>("rating");
+		ratingColumn.setPrefWidth(90);
+		ratingColumn.setCellValueFactory(new PropertyValueFactory<>("rating"));
+		
+		
+       slotsTable.getSelectionModel().selectedItemProperty().addListener((observableValue, oldValue, newValue) -> {
+    	   
+            if (slotsTable.getSelectionModel().getSelectedItem() != null) {
+            	if(oldValue != null){ 
+            		engine.executeScript("setUnSelected('" + oldValue.getName() + "');");	
+            	}
+            	engine.executeScript("setSelected('" + newValue.getName()  + "');");	
+            	engine.executeScript("refresh()");
+            }
+        });
+	}
+	
+	private int roundMinutes(int minutes, boolean roundUp){
+		if (0 < minutes && minutes < 15){
+			if(roundUp)
+				return 15;
+			return 0;
+		}
+		if (15 < minutes && minutes < 30){
+			if(roundUp)
+				return 30;
+			return 15;
+		}
+		if (30 < minutes && minutes < 45){
+			if(roundUp)
+				return 45;
+			return 30;
+		}
+		if (45 < minutes && minutes < 60){
+			if(roundUp)
+				return 60;
+			return 45;
+		}
+		return minutes;
+	}
+	
+	public ObservableList<PresentParkingSlot> getSlots(List<PresentParkingSlot> slots){
+		
+		ObservableList<PresentParkingSlot> returnSlots = FXCollections.observableArrayList();
+		for (PresentParkingSlot slot : slots){
+			returnSlots.add(slot);
+		}
+		return returnSlots;
+	}
+	public void setUserId(String id){
+		this.userId = id;
+	}
+	private ParseGeoPoint getSelectedDestination(){
+		
+		if(studentHouseRadioButton.isSelected()){
+			return new ParseGeoPoint(32.776423, 35.022825);
+		}
+		if(ulmanRadioButton.isSelected()){
+			return new ParseGeoPoint(32.777100, 35.023770);
+       	}
+		if(taubRadioButton.isSelected()){
+			return new ParseGeoPoint(32.777668, 35.021486);
+		}
+		//sportsCenterRadioButton isSelected
+		return new ParseGeoPoint(32.779119, 35.019112);
+	}
 }
 
 
