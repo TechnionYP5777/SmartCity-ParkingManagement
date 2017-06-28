45259392fc53b4ce23a04700563fa52827193bbe
Toma <tom.nof@gmail.com>
2017-06-28 11:03:25 +0300

    ComibineGraphTest: add test to the new graph

--- src/test/java/logic/CombineGraphTest.java
+++ src/test/java/logic/CombineGraphTest.java
@@ -14,7 +14,7 @@ import org.parse4j.ParseObject;
 import data.management.DatabaseManager;
 import util.Log;
 
-public class GraphTest {
+public class CombineGraphTest {
 	
 	private static DatabaseManager dbm = Mockito.mock(DatabaseManager.class);
 	private static List<ParseObject> parkingSlots = new ArrayList<>();
@@ -33,16 +33,19 @@ public class GraphTest {
 		p1.put("name", "testSlot1");
 		p1.put("location",new ParseGeoPoint(32.777745,35.024));
 		p1.put("rank", 1);
+		p1.put("rating", 1);
 		
 		ParseObject p2 = new ParseObject("ParkingSlot");
 		p2.put("name", "testSlot2");
 		p2.put("location",new ParseGeoPoint(32.777907,35.023828));
 		p2.put("rank", 2);
+		p2.put("rating", 2);
 		
 		ParseObject p3 = new ParseObject("ParkingSlot");
 		p3.put("name", "testSlot3");
 		p3.put("location",new ParseGeoPoint(32.778015,35.021982));
 		p3.put("rank", 3);
+		p3.put("rating", 3);
 		
 		// insert all slots to array
     	parkingSlots.add(p1);
@@ -57,6 +60,8 @@ public class GraphTest {
 		PopulateMokito();
 		Assert.assertEquals(3,
 				(new Graph(dbm)).CreatePriceDistanceData(new ParseGeoPoint(32.777533, 35.024231)).size());
+		Assert.assertEquals(3,
+						(new Graph(dbm)).CreatePriceRanttingData(new ParseGeoPoint(32.777533, 35.024231)).size());
 	}
 	
 	@Test
@@ -66,5 +71,7 @@ public class GraphTest {
 		PopulateMokito();
 		Assert.assertEquals(3,
 				(new Graph(dbm)).CreatePriceDistanceData(new ParseGeoPoint(32.777533, 35.024231)).size());
+		Assert.assertEquals(3,
+				(new Graph(dbm)).CreatePriceRanttingData(new ParseGeoPoint(32.777533, 35.024231)).size());
 	}
 }
